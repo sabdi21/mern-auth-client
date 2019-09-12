fb2fae8294351cd579da630727c025556eb572a5
sabdi21 <sabdi21@hotmail.com>
2019-09-12 12:43:34 -0700

    Routes added

---
+++ src/content/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route} from 'react-router-dom'
+
+// Importing routes 
+import Home from '../pages/Home'
+import Profile from '../pages/Profile'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+
+const Content = props => {
+    return (
+        <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/Signup" component={Signup} />
+        </div>
+    )
+}
+
+export default Content
---
+++ src/pages/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Signup = props => {
+    return (
+        <h1>Signup Content</h1>
+    )
+}
+
+export default Signup
